73d9b80d6fa1cf75dba73e396d1f5d3bd4963df6
sevenbigcat <sevenbigcat@users.noreply.github.com>
2017-06-14 16:58:59 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wthen',
+  packages = ['wthen'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A simple rule engine with YAML format',
+  author = 'Alex Yu',
+  author_email = 'mltest2000@aliyun.com',
+  url = 'https://github.com/sevenbigcat/wthen', # use the URL to the github repo
+  download_url = 'https://github.com/sevenbigcat/wtehn/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['rule engine', 'ECA', 'YAML'], # arbitrary keywords
+  classifiers = [],
+)
